b1136048d37383cfdc87096ee15dd5e76aa83c9a
jhonataguerra <jhonataguerra@outlook.com.br>
2022-11-28 19:08:34 -0300

    Decorator de teste

--- ecommerce/apps/users/api/api.py
+++ ecommerce/apps/users/api/api.py
@@ -1,10 +1,22 @@
-from rest_framework.views import APIView, Response
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+
 from apps.users.models import User
 from apps.users.api.serializers import UserSerializer
 
 
-class UserAPIView(APIView):
-    def get(self, request):
-        users = User.objects.all()
-        users_serializers = UserSerializer(users, many=True)
-        return Response(users_serializers.data)
+@api_view(["GET"])
+def user_api_view(request):
+    def get(request):
+        if request.method == "GET":
+            users = User.objects.all()
+            users_serializers = UserSerializer(users, many=True)
+            return Response(users_serializers.data)
+
+        elif request.method == "POST":
+            users_serializer = UserSerializer(data=request.data)
+            if users_serializer.is_valid():
+                users_serializer.save()
+                return Response(users_serializer.data)
+            return Response(users_serializer.errors)
