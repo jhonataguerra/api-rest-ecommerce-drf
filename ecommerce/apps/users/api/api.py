ff7acb7e1243d969ce3ca7f50c2154b74cf4897d
jhonataguerra <jhonataguerra@outlook.com.br>
2022-11-30 12:29:29 -0300

    testes de métodos put e delete

--- ecommerce/apps/users/api/api.py
+++ ecommerce/apps/users/api/api.py
@@ -6,7 +6,7 @@ from apps.users.models import User
 from apps.users.api.serializers import UserSerializer
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def user_api_view(request):
     def get(request):
         if request.method == "GET":
@@ -20,3 +20,22 @@ def user_api_view(request):
                 users_serializer.save()
                 return Response(users_serializer.data)
             return Response(users_serializer.errors)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def user_detail_api_view(request, pk=None):
+
+    if request.method == "GET":
+        user = User.objects.filter(id=pk).first()
+        user_serializer = UserSerializer(user)
+        return Response(user_serializer.data)
+
+    elif request.method == "PUT":
+        request.data
+        user = User.objects.filter(id=pk).first()
+        user_serializer = UserSerializer(user, data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data)
+
+        return Response(user_serializer.errors)
